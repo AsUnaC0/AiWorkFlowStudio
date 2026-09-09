0626cf71c21c521f67d7f5a58c75be902836ba93
yuancheng <1921859408@qq.com>
2026-09-09 10:10:41 +0800

    Initial commit: AiWorkFlowStudio frontend

---
+++ fronted/vite.config.ts
@@ -0,0 +1,26 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { TDesignResolver } from '@tdesign-vue-next/auto-import-resolver'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [
+        TDesignResolver({
+          library: 'vue-next',
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        TDesignResolver({
+          library: 'vue-next',
+        }),
+      ],
+    }),
+  ],
+})
